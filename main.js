0de956b6a3d725df380789f695680c5534c85446
adityamudgil2505 <adityamudgil2505@gmail.com>
2019-09-04 02:13:11 +0530

    Added first screen

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
-const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+const electron = require('electron');
+const {app, BrowserWindow, Menu, Tray}=electron;
+const iconPath = path.join(__dirname, 'logo.png');
 let win;
-
 function createWindow(){
-  win = new BrowserWindow({
-    width: 500, 
-    height: 550,
-    show: false
-  });
-  // win.loadURL('http://github.com');
-  win.loadURL(`file://${__dirname}/dist-web/index.html`)
-  win.once('ready-to-show', win.show);
-  win.on('closed', ()=> win=null);  
+  win = new BrowserWindow({show:false, height:650, width:1100, frame:false, resizable:false, webPreferences: {
+    nodeIntegration: true
+    }});
+  win.loadURL(url.format({
+    pathname:path.join(__dirname, '/src/index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+  // win.webContents.openDevTools();
+  win.on('closed', ()=> win=null);
+  win.once('ready-to-show', ()=>{
+    win.show();
+  })
 }
-
 app.on('ready', createWindow);
-app.on('window-all-closed',()=>{
-  if(process.platform!=='darwin'){  app.quit();}
-})
-app.on('activate', function(){
-  if(win===null){ createWindow();};
+// app.on('ready', ()=>{
+//   new Tray(iconPath);
+// });
+
+// For Mac
+app.on('window-all-closed', ()=>{
+  if(process.platform!=='darwin'){
+    app.quit();
+  }
 });
+
+app.on('activate', ()=>{
+  if(win===null){
+    createWindow();
+  }
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,16 @@ import 'rxjs/add/operator/do';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // require('dotenv').config();
+  // const request = require('request');
+  // const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}`
+
+  // const title = document.getElementById("title");
+  // const description = document.getElementById("description")
+
+  // request.get(url, (error, response, body)=>{
+  //   body = JSON.parse(body);
+  //   title.innerHTML = body.articles[0].title;
+  //   description.innerHTML = body.articles[0].description;
+  // })
 }
