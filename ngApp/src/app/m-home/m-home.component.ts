07e928e3b5e4ab9c722e9784a0f652bba62a340b
adityamudgil2505 <adityamudgil2505@gmail.com>
2019-10-10 00:15:41 +0530

    Redirect successfuly to view news mode

--- ngApp/src/app/m-home/m-home.component.ts
+++ ngApp/src/app/m-home/m-home.component.ts
@@ -12,12 +12,9 @@ export class MHomeComponent implements OnInit {
   constructor(private apiService: ApiKeyTestingService, private _sanitizer: DomSanitizer, private router: Router, private route: ActivatedRoute){ }
   public news:any=[];
   viewNews($event, newValue){
-    console.log("You clicked");
-    console.log(newValue);
-    // newValue.image=this._sanitizer.bypassSecurityTrustStyle(`url(${newValue.urlToImage})`);
+    newValue.image=this._sanitizer.bypassSecurityTrustStyle(`url(${newValue.urlToImage})`);
     let dt:moment.Moment= moment(newValue.publishedAt);
     newValue.date= dt.format('LLL');
-
     this.apiService.addNewsToRecent(newValue);
     this.router.navigate(["view"], {relativeTo: this.route.parent});
   }
