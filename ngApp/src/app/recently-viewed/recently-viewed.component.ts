2ed05357d835c3445929195df9cfb392903068d1
adityamudgil2505 <adityamudgil2505@gmail.com>
2019-10-10 00:58:56 +0530

    View more of recent news done

--- ngApp/src/app/recently-viewed/recently-viewed.component.ts
+++ ngApp/src/app/recently-viewed/recently-viewed.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiKeyTestingService} from '../api-key-testing.service';
 import { DomSanitizer} from '@angular/platform-browser';
+import { Router, ActivatedRoute} from '@angular/router';
 import { from } from 'rxjs';
 @Component({
   selector: 'app-recently-viewed',
@@ -8,16 +9,21 @@ import { from } from 'rxjs';
   styleUrls: ['./recently-viewed.component.scss']
 })
 export class RecentlyViewedComponent implements OnInit {
-  constructor(private apiService:ApiKeyTestingService, private _sanitizer:DomSanitizer) { }
+  constructor(private apiService:ApiKeyTestingService, private _sanitizer:DomSanitizer, private router: Router, private route: ActivatedRoute) { }
   
   public windTitle:String="Recently Viewed";
   recentArticle:any=[];
 
+  viewNews($event, newValue){
+    newValue.image=this._sanitizer.bypassSecurityTrustStyle(`url(${newValue.urlToImage})`);
+    this.apiService.addNewsToRecent(newValue);
+    this.router.navigate(["view"], {relativeTo: this.route.parent});
+  }
+
   ngOnInit() {
     this.recentArticle=this.apiService.getDetails().recent;
     for(let i=0; i<this.recentArticle.length; i++){
-      this.recentArticle[i].urlToImage = this._sanitizer.bypassSecurityTrustStyle(`url(${this.recentArticle[i].urlToImage})`);
-      this.recentArticle[i].source = this.recentArticle[i].source.name;
+      this.recentArticle[i].image = this._sanitizer.bypassSecurityTrustStyle(`url(${this.recentArticle[i].urlToImage})`);
     }
   }
 
