852ea914db5dfedaa74820580a605de195f3ae3f
adityamudgil2505 <adityamudgil2505@gmail.com>
2019-10-18 20:14:44 +0530

    Finishing touch

--- ngApp/main.js
+++ ngApp/main.js
@@ -16,14 +16,16 @@ var notifTitle="";
 let win;
 function createWindow(){
   win = new BrowserWindow({show:false, height:650, width:1100, titleBarStyle: 'hiddenInset', resizable:false, webPreferences: {
-    nodeIntegration: true
+    nodeIntegration: true,
+    webSecurity: false
     }});
+  win.setFullScreenable(false);
   win.loadURL(url.format({
     pathname:path.join(__dirname, '/dist/ngApp/index.html'),
     protocol: 'file',
     slashes: true
   }));  
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
   win.on('closed', ()=> win=null);
   win.once('ready-to-show', ()=>{
     win.show();
